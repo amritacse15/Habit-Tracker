96091abfebef14146737bda41910eedfecd74d69
amritacse15 <113686859+amritacse15@users.noreply.github.com>
2023-12-18 00:17:34 +0530

    habit tracker created

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import Header from './components/Header'
+import Overview from './components/Overview'
+import HabitDisplay from './components/HabitDisplay'
+
+import { Row, Col, Container } from 'react-bootstrap'
+import { useEffect } from 'react'
+
+import Typed from 'typed.js'
+
+function App() {
+  // code for typed.js library
+  useEffect(() => {
+    const typed = new Typed('.tag', {
+      strings: [
+        "Hi i am amrita singh , I created a Habit tracker , by using react , jsx and vanilla Js, react-redux",
+      ], // Strings to display
+      // Speed settings, try diffrent values untill you get good results
+      startDelay: 2000,
+      typeSpeed: 40,
+      backSpeed: 20,
+      backDelay: 5000,
+      loop: true,
+    })
+
+    // Destroying
+    return () => {
+      typed.destroy()
+    }
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <Container>
+        <Row className='mt-5'>
+          <Col md={3}>
+            {/* Overview component */}
+            <Overview />
+          </Col>
+          <Col md={9}>
+            {/*  HabitDetails Component*/}
+            <HabitDisplay />
+          </Col>
+        </Row>
+        <h4 className='text-light d-flex  mt-5  justify-content-center'>
+          <span className='tag'></span>
+        </h4>
+      </Container>
+    </>
+  )
+}
+
+export default App
